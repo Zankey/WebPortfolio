2c485612f6decd69d6bacf801fdbc12ed7cd9c89
max volz <maxwellvolz@gmail.com>
2019-02-18 17:27:39 -0800

    appbar

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import GameCards from './GameCards';
+import AppBar from './AppBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-         
+
+        <AppBar />
         <GameCards />
 
-        </header>
 
       </div>
     );
